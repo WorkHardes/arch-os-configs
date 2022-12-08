ac858fde705f4889d4e2b4c458655a8e018278af
WorkHardes <arckadyor34@gmail.com>
2022-12-08 10:22:08 +0300

    feat: nvim theme

--- set-configs.sh
+++ set-configs.sh
@@ -6,14 +6,28 @@ lf_config_folder_path="~/.config/lf/"
 kitty_config_file_path="~/.config/kitty/kitty.conf"
 k9s_config_folder_path="~/.config/k9s/"
 nvim_config_folder_path="~/.config/nvim/"
+nvim_theme_folder_path=" ~/.local/share/nvim/site/autoload/airline/theme"
 
-# Setting configurations
-cat ./dotfiles/alacritty/alacritty.yml >> alacritty_config_file_path
-cp ./dotfiles/lf/* lf_config_folder_path
-cat ./dotfiles/kitty/kitty.conf >> kitty_config_file_path
-cp ./dotfiles/k9s/skin.yml k9s_config_folder_path
-cat ./dotfiles/zshrc >> zsh_config_file_path
-cp ./dotfiles/nvim/init.vim nvim_config_file_path
+# alacritty
+cat ./dotfiles/alacritty/alacritty.yml >> $alacritty_config_file_path
+
+# lf
+cp ./dotfiles/lf/* $lf_config_folder_path
+
+# kitty
+cat ./dotfiles/kitty/kitty.conf >> $kitty_config_file_path
+
+# k9s
+cp ./dotfiles/k9s/skin.yml $k9s_config_folder_path
+
+# zsh
+cat ./dotfiles/zshrc >> $zsh_config_file_path
+
+# nvim
+cp ./dotfiles/nvim/init.vim $nvim_config_folder_path
+mkdir -p $nvim_theme_folder_path
+cp ./dotfiles/nvim/tokyonight.vim $nvim_theme_folder_path
+
 # tmux
 cp -r ./dotfiles/tmux/ ~/
 cd
